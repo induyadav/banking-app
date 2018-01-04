c94b97ebdb71e562d906ee4bdd135038e3ac9440
Ashutosh Mane <ashutosh@ashutoshs-macbook-pro.local>
2018-01-04 14:08:46 +0530

    removed buttons

--- banking app/ViewController.swift
+++ banking app/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //comment added by ashu
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
