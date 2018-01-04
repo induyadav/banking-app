bec9163ae7ad26d334a6fc4c0626737c2c1060b4
Indu Yadav <induyadav018@gmail.com>
2018-01-04 13:22:42 +0530

    Initial Commit

---
+++ banking app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  banking app
+//
+//  Created by Indu Yadav on 04/01/18.
+//  Copyright © 2018 Indu Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
