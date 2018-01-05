ea8c41eba90816d9f4630b9e04e4f53429a209d9
Indu Yadav <induyadav018@gmail.com>
2018-01-05 22:40:50 +0530

    transaction main storyboard sorted

---
+++ banking app/TransactionListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TransactionListViewController.swift
+//  banking app
+//
+//  Created by Indu Yadav on 05/01/18.
+//  Copyright © 2018 Indu Yadav. All rights reserved.
+//
+
+import UIKit
+
+class TransactionListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
