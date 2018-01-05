361b8990b8acc6aad4956c7af736ac0c227e6fd7
Indu Yadav <induyadav018@gmail.com>
2018-01-05 22:27:36 +0530

    segue to transaction view controller

---
+++ banking app/AccountTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  AccountTableViewCell.swift
+//  banking app
+//
+//  Created by Indu Yadav on 04/01/18.
+//  Copyright © 2018 Indu Yadav. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableViewCell: UITableViewCell{
+  
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var setAccountBalance: UILabel!
+    @IBOutlet weak var setAccountNumber: UILabel!
+    @IBOutlet weak var setBankName: UILabel!
+    @IBOutlet weak var setName: UILabel!
+    
+    setAccountBalance.text = Account.name
+    
+    
+}
--- banking app/AccountTableViewController.swift
+++ banking app/AccountTableViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class AccountTableViewController: UITableViewController {
+class AccountTableViewController: UITableViewController,UITableViewDataSource{
     
     var accounts : [Account] = []
-    
+    var myIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,14 +29,28 @@ class AccountTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+  
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        myIndex = indexPath.row
+        performSegue(withIdentifier: "segue", sender: self)
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
-     
-        return 1
+     return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return accounts.count
     }
+  
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AccountCell", for: IndexPath)as! AccountTableViewCell
+        let index = indexPath.row
+        let account = accounts[index]
+        cell.
+        
+    }
+    
     @IBAction func cancelAccountCreation(_ segue: UIStoryboardSegue) {}
     @IBAction func saveAccount(_ segue: UIStoryboardSegue) {}
   
